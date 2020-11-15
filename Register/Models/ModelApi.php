4780413054504981cc9d295fdffbff7c48fc72d2
Alessandro Ayrton Grassi <alexgrassi181199@gmail.com>
2020-11-15 20:52:58 +0100

    ModeApi

--- Register/Models/ModelApi.php
+++ Register/Models/ModelApi.php
@@ -1,6 +1,39 @@
 <?php
 
 
+require_once "../../DBMSR/Connection.php";
+
 class ModelApi {
 
+    /**
+     * helper function to insert into db
+     * @param array ...$data
+     * @return int
+     */
+    public function insert(...$data){
+        global $con;
+        $statement = $con->prepare("INSERT INTO client_users (fname, lname, email, password,date) VALUES(?, ?, ?, ?,now())");
+        $statement->execute(array(
+            $data['fname'],
+            $data['lname'],
+            $data['email'],
+            Utils::createToken($data['password'])
+        ));
+
+        return $statement->rowCount();
+    }
+
+
+    /**
+     * helper function to get user if exists
+     * @param $email String
+     * @return bool value
+     */
+    public function isExistsUser(string $email){
+        global $con;
+        $statement = $con->prepare("SELECT email FROM client_users WHERE email = ?");
+        $statement->execute(array($email));
+
+        return $statement->rowCount();
+    }
 }
